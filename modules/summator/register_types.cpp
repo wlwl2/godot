feff3ef945f28ff36e2b63a9c815277d1c202f67
William Lim <manvinedi@gmail.com>
2022-11-07 22:30:18 +0800

    summator

---
+++ modules/summator/register_types.cpp
@@ -0,0 +1,12 @@
+#include "register_types.h"
+
+#include "core/class_db.h"
+#include "summator.h"
+
+void register_summator_types() {
+	ClassDB::register_class<Summator>();
+}
+
+void unregister_summator_types() {
+	// Nothing to do here in this example.
+}
---
+++ modules/summator/register_types.h
@@ -0,0 +1,2 @@
+void register_summator_types();
+void unregister_summator_types();
